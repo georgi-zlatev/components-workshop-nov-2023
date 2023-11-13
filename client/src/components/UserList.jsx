fd329192c4698673c983d169d71e9b457dfd03b2
Georgi Zlatev <georgi.n.zlatev@gmail.com>
2023-11-13 22:45:39 +0200

    Trimmed a template for loading and overlapping from userlist table component and put it back in the index template for later use

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -6,6 +6,8 @@ const UserList = () => {
   <section className="card users-container">
     <Search/>
     <UserListTable/>
+    {/* <!-- New user button  --> */}
+      <button className="btn-add btn">Add new user</button>
   </section>
   )
 };
